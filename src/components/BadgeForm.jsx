3ed70a679f7283901b4c1d7a9a5e63598da0fbab
Julián Melero Hidalgo <dev@julianmelero.dev>
2021-04-13 11:20:18 +0200

    setState Forms

--- src/components/BadgeForm.jsx
+++ src/components/BadgeForm.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 
 class BadgeForm extends React.Component {
+  // Iniciamos los valores de los formularios
+  state = {};
   handleChange = (e) => {
     console.log({
-      name: e.target.name,
-      value: e.target.value,
+      /*    name: e.target.name,
+      value: e.target.value,*/
+    });
+    this.setState({
+      [e.target.name]: e.target.value,
     });
   };
   handleClick = (e) => {
@@ -13,26 +18,62 @@ class BadgeForm extends React.Component {
 
   handleSubmit = (e) => {
     e.preventDefault();
-    console.log("Form was submit");
+    console.log("Form was submitted");
+    console.log(this.state);
   };
+
   render() {
     return (
       <div>
         <h1>New Attendant</h1>
 
         <form onSubmit={this.handleSubmit}>
-          <label htmlFor="">First Name</label>
+          <label htmlFor="firstName">First Name</label>
           <input
             onChange={this.handleChange}
             type="text"
             name="firstName"
-            id=""
+            id="firstName"
+            value={this.state.firstName}
+          />
+          <label htmlFor="lastName">Last Name</label>
+          <input
+            onChange={this.handleChange}
+            type="text"
+            name="lastName"
+            id="lastName"
+            value={this.state.lastName}
+          />
+          <label htmlFor="email">Email</label>
+          <input
+            onChange={this.handleChange}
+            type="email"
+            name="email"
+            id="email"
+            value={this.state.email}
+          />
+          <label htmlFor="jobTitle">Job Title</label>
+          <input
+            onChange={this.handleChange}
+            type="text"
+            name="jobTitle"
+            id="jobTitle"
+            value={this.state.jobTitle}
+          />
+          <label htmlFor="jobTitle">Twitter</label>
+          <input
+            onChange={this.handleChange}
+            type="text"
+            name="twitter"
+            id="twitter"
           />
           <button
-            type="submit"                        
+            type="submit"
             onClick={this.handleClick}
             className="btn btn-primary"
-          ></button>
+          >
+            Enviar
+          </button>
         </form>
       </div>
     );
